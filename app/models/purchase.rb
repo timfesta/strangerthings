a08b102c0e1743be3294c6f1c2b4acfa37a33128
timfesta <timothyfesta@yahoo.com>
2017-02-04 17:35:42 -0800

    uuid

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,6 @@
 class Purchase < ApplicationRecord
+
+	def to_param
+		uuid
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204233308) do
+ActiveRecord::Schema.define(version: 20170205013010) do
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170204233308) do
     t.integer  "product_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "uuid"
   end
 
 end
