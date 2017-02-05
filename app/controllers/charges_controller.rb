ee743ebac5a81f4a95fc91ba9663078e8e1d03d8
timfesta <timothyfesta@yahoo.com>
2017-02-05 13:35:01 -0800

    stripe payments

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,18 +6,19 @@ class ChargesController < ApplicationController
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :source  => params[:stripeToken]
+	    :source  => params[:stripeToken],
+	    :plan => "CAT"
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => product.price_in_cents,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+	  # charge = Stripe::Charge.create(
+	  #   :customer    => customer.id,
+	  #   :amount      => product.price_in_cents,
+	  #   :description => 'Rails Stripe customer',
+	  #   :currency    => 'usd'
+	  # )
 
 	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
-	  	amount: product.price_in_cents, description: charge.description, currency: charge.currency,
+	  	amount: product.price_in_cents, description: product.description, currency: "usd",
 	  	customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
 
 	  redirect_to purchase
